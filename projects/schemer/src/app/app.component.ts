02dece0c6c019974b795b29fea2316be74fe2647
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-06-17 15:46:29 +0200

    Subscribe to schemer variable changes
    Add verona api event and subscription to AppComponent

--- projects/schemer/src/app/app.component.ts
+++ projects/schemer/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VeronaAPIService, VosStartCommand } from './services/verona-api.service';
+import { MetaDataService } from './services/meta-data.service';
+import { MainDataService } from './services/main-data.service';
 
 @Component({
   selector: 'app-root',
   template: `
     <mat-drawer-container class="mainView" fxLayout="column" autosize hasBackdrop="false">
-      <schemer-toolbar *ngIf="isStandalone()" (toggleDrawerClick)="drawer.toggle()"></schemer-toolbar>
+      <schemer-toolbar *ngIf="isStandalone" (toggleDrawerClick)="drawer.toggle()"></schemer-toolbar>
       <div fxLayout="row">
         <schemer-stage fxFlex></schemer-stage>
         <mat-drawer #drawer mode="over" fxFlex="400px" fxLayout="column">
@@ -21,7 +24,32 @@ import { Component } from '@angular/core';
   styles: [
     '.mainView {height: 100%;}'
   ] })
-export class AppComponent {
-  isStandalone = (): boolean => window === window.parent;
+export class AppComponent implements OnInit {
+  isStandalone: boolean = window === window.parent;
   title = 'schemer';
+
+  constructor(private veronaAPIService: VeronaAPIService,
+              private metaDataService: MetaDataService,
+              private mainDataService: MainDataService) {
+  }
+
+  ngOnInit(): void {
+    this.veronaAPIService.sendVosReadyNotification(this.metaDataService.metadata);
+    this.veronaAPIService.vosStartCommand
+      .subscribe((message: VosStartCommand) => this.setVariables(message));
+  }
+
+  private setVariables(message: VosStartCommand): void {
+    this.mainDataService.selectedScheme$.next(null);
+    this.mainDataService.varList = message.variables;
+    this.mainDataService.codingSchemes = [];
+    this.mainDataService.varList.sort(function (a, b) {
+      const idA = a.id.toUpperCase();
+      const idB = b.id.toUpperCase();
+      if (idA < idB) return -1;
+      if (idA > idB) return 1;
+      return 0;
+    });
+    this.mainDataService.syncVariables();
+  }
 }
