541ecfcd65fe3022add77c9e8572fd15697be354
mechtelm <mechtelqiqb.hu-berlin.de>
2023-12-03 10:34:52 +0100

    Remove response scheme and autocoder
    - update to angular 16.2

--- projects/auto-coder/src/public-api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of auto-coder
- */
-
-export * from './lib/auto-coder.class';
--- projects/response-scheme/src/public-api.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/classes/variable-info';
-export * from './lib/classes/variable-coding-data';
-export * from './lib/classes/response';
